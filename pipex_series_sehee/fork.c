e05fc1debbab9121449c71fd5f26d50cbdf8d9d3
홍세희 <shhong3317@gmail.com>
2021-12-04 08:11:53 +0900

    fork for pipe

--- pipex_series_sehee/fork.c
+++ pipex_series_sehee/fork.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 //fork, pipe, dup, dup2, wait, waitpid, exit, execve, unlink
 
 int	main(void)
@@ -8,13 +8,15 @@ int	main(void)
 
 	pid = fork();
 	if (pid == -1)
-	{
-		
-	}
-	else if (!pid)
-	{
+		printf("failed to call fork().\n");
+	else if (!pid)	//자식 부모세스(새로 생성한 프로세스)
+	{	
 		printf("hello. I'm a child process.\n");
-		return (0);
 	}
-
+	else			//부모 프로세스
+	{	
+		sleep(5);
+		printf("I'm a parent process.\n");
+	}
+	return (0);
 }
