10d6beda2ea81df43b58722d57efb3ed1b82cbdd
홍세희 <shhong3317@gmail.com>
2021-12-04 08:12:12 +0900

    fork for pipe

--- pipex_series_sehee/fork.c
+++ pipex_series_sehee/fork.c
@@ -11,11 +11,11 @@ int	main(void)
 		printf("failed to call fork().\n");
 	else if (!pid)	//자식 부모세스(새로 생성한 프로세스)
 	{	
-		printf("hello. I'm a child process.\n");
+		printf("I'm a child process.\n");
 	}
 	else			//부모 프로세스
 	{	
-		sleep(5);
+		sleep(5);	//
 		printf("I'm a parent process.\n");
 	}
 	return (0);
